5189407c77e30ff5df310873f8e08d6bf8e1fce8
potemaa <164544097+potemaa@users.noreply.github.com>
2024-03-24 00:01:48 +0300

    3tyler.py

--- 3tyler.py
+++ 3tyler.py
@@ -3,8 +3,8 @@ def is_prime(x):#функция проверки простоты числа
         if x % i == 0:
             return False
     return True
-for x in range(1,5):#перебираем возможные цифры в записи операнд
-	for y in range(1,5):
+for x in range(1,4):#перебираем возможные цифры в записи операнд
+	for y in range(1,4):
 		res=x+6*7+x*(7**2)+y*(7**3)+y*(7**4)+2*(7**5)+y+(x*5)+y*(5**2)+3*(5**3)+2*(5**4)+3*(5**5)
 		if is_prime(res):
 			sc=sum([int(a) for a in str(res)])#сумма цифр
